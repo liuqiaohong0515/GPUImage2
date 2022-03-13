08e99d5dc3618a231018e505134f2f6cb9f4eb96
liuqiaohong0515 <liuqiaohong0515@gmail.com>
2022-03-13 18:45:08 +0800

    fix ios showcase can't open camera when ios version over ios14

--- examples/iOS/FilterShowcase/FilterShowcaseSwift/FilterDisplayViewController.swift
+++ examples/iOS/FilterShowcase/FilterShowcaseSwift/FilterDisplayViewController.swift
@@ -9,25 +9,25 @@ class FilterDisplayViewController: UIViewController, UISplitViewControllerDelega
     @IBOutlet var filterSlider: UISlider?
     @IBOutlet var filterView: RenderView?
     
-    let videoCamera:Camera?
+    var videoCamera:Camera?
     var blendImage:PictureInput?
 
     required init(coder aDecoder: NSCoder)
     {
-        do {
-            videoCamera = try Camera(sessionPreset:.vga640x480, location:.backFacing)
-            videoCamera!.runBenchmark = true
-        } catch {
-            videoCamera = nil
-            print("Couldn't initialize camera with error: \(error)")
-        }
-
         super.init(coder: aDecoder)!
     }
     
     var filterOperation: FilterOperationInterface?
     
     func configureView() {
+        do {
+            videoCamera = try Camera(sessionPreset:.hd1280x720, location:.backFacing)
+            videoCamera!.runBenchmark = true
+        } catch {
+            videoCamera = nil
+            print("Couldn't initialize camera with error: \(error)")
+        }
+        
         guard let videoCamera = videoCamera else {
             let errorAlertController = UIAlertController(title: NSLocalizedString("Error", comment: "Error"), message: "Couldn't initialize camera", preferredStyle: .alert)
             errorAlertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default, handler: nil))
@@ -52,8 +52,6 @@ class FilterDisplayViewController: UIViewController, UISplitViewControllerDelega
                 case let .custom(filterSetupFunction:setupFunction):
                     currentFilterConfiguration.configureCustomFilter(setupFunction(videoCamera, currentFilterConfiguration.filter, view))
                 }
-                
-                videoCamera.startCapture()
             }
 
             // Hide or display the slider, based on whether the filter needs it
@@ -86,6 +84,11 @@ class FilterDisplayViewController: UIViewController, UISplitViewControllerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoCamera?.startCapture()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
